16b71bfcd196c316acf7116ee56d29b0677a1f84
Florian Lercher <florian.lercher@tum.de>
2021-09-22 18:17:04 +0000

    Fix some lint errors

--- WebService/Sources/Demo/hello.swift
+++ WebService/Sources/Demo/hello.swift
@@ -1,7 +1,7 @@
 import Apodini
 import Foundation
 
-struct mItems: Content, Decodable {
+struct MeasurementItem: Content, Decodable {
     var sensorID: Int
     var sensorType: Int
     var measurement: Double
@@ -11,7 +11,7 @@ struct JSONStructure: Content, Decodable {
     var buoyId: Int
     var date: String
     var location: Location
-    var measurements: [mItems]
+    var measurements: [MeasurementItem]
 }
 
 struct Location: Content, Decodable {
@@ -20,29 +20,9 @@ struct Location: Content, Decodable {
 }
 
 struct Hello: Handler {
-
     func handle() -> JSONStructure {
-        
-        // Do it like this 
         let contents = try! JSONDecoder().decode(JSONStructure.self, from: try! Data(contentsOf: URL(fileURLWithPath: "/buoy/test.json")))
         
         return contents
-
     }
 }
-
-
-/*func convertStringToDictionary(text: String) -> [String:String] {
-    
-    var result: [String:String] = [:] 
-    if let data = text.data(using: .utf8) {
-         
-        do {
-            result = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String:String]
-        } catch {
-            result["Error"] = "Invalid JSON"
-        }
-    }
-
-    return result
-}*/
