9c1bdae34c7e2a848e052454ce9dda0e204a631d
Florian Lercher <florian.lercher@tum.de>
2021-09-22 12:32:45 +0000

    Remove http config

--- WebService/Sources/Demo/Demo.swift
+++ WebService/Sources/Demo/Demo.swift
@@ -8,16 +8,15 @@ import ArgumentParser
 struct DemoWebService: WebService {
     @Option(help: "The port the web service is offered at")
     var port: Int = 80
-    
-    
+
+
     var configuration: Configuration {
-        HTTPConfiguration(port: port)
         HTTP2Configuration(cert: "/buoy/cert.pem", keyPath: "/buoy/key.pem")
         REST {
             OpenAPI()
         }
     }
-    
+
     var content: some Component {
         Group("sensors") {
             Sensor()
