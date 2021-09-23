80523d5947eee9542bce2bcf7f0ecdce10fddc06
Florian Lercher <florian.lercher@tum.de>
2021-09-23 11:55:53 +0000

    Read available sensors from config files

--- WebService/Sources/Demo/Sensor.swift
+++ WebService/Sources/Demo/Sensor.swift
@@ -1,17 +1,12 @@
 import Apodini
+import Foundation
 
 
 struct Sensor: Handler {
-    func handle() -> String {
-        """
-        List of sensors:
-        pH
-        """
-    }
-
-    var content: some Component {
-        Group("ph") {
-            PhSensor()
+    func handle() -> [Int] {
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: "/buoy/available_sensors.json")) else {
+            return []
         }
+        return (try? JSONDecoder().decode([Int].self, from: data)) ?? []
     }
 }
