d4a73ee371dace3c076186833c631c38cfbf7893
Florian Lercher <florian.lercher@tum.de>
2021-09-26 08:40:27 +0000

    Return sensor name from sensors endpoint

--- WebService/Sources/Buoy/Sensor.swift
+++ WebService/Sources/Buoy/Sensor.swift
@@ -3,7 +3,17 @@ import Foundation
 
 
 struct Sensor: Handler {
-    func handle() -> [Int] {
-        readJSONFromFile([Int].self, filePath: "available_sensors.json") ?? []
+    func handle() -> [SensorDescription] {
+        readJSONFromFile([SensorType].self, filePath: "available_sensors.json")?.map { SensorDescription(type: $0) } ?? []
+    }
+}
+
+struct SensorDescription: Content {
+    let name: String
+    let type: SensorType
+
+    init(type: SensorType) {
+        self.type = type
+        self.name = type.description
     }
 }
