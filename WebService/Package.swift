0804b66fc572862adc3987d2efcbfcc7635a4d5a
Florian Lercher <florian.lercher@tum.de>
2021-09-30 11:25:25 +0000

    Add IoT runtime

--- WebService/Package.swift
+++ WebService/Package.swift
@@ -16,15 +16,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.5.0")),
-        .package(name: "buoy-deployment-provider", url: "https://github.com/fa21-collaborative-drone-interactions/buoy-deployment-provider", .branch("develop"))
+        .package(name: "buoy-deployment-provider", url: "https://github.com/fa21-collaborative-drone-interactions/buoy-deployment-provider", .branch("develop")),
+        .package(name: "ApodiniIoTDeploymentProvider", url: "https://github.com/Lerbert/ApodiniIoTDeploymentProvider", .branch("docker-compose"))
     ],
     targets: [
         .executableTarget(
             name: "Buoy",
             dependencies: [
                 .product(name: "Apodini", package: "Apodini"),
+                .product(name: "ApodiniDeploy", package: "Apodini"),
                 .product(name: "ApodiniHTTP", package: "Apodini"),
-                .product(name: "BuoyDeploymentOption", package: "buoy-deployment-provider")
+                .product(name: "BuoyDeploymentOption", package: "buoy-deployment-provider"),
+                .product(name: "DeploymentTargetIoTRuntime", package: "ApodiniIoTDeploymentProvider")
             ]
         ),
         .testTarget(
